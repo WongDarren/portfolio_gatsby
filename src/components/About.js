6691b47318db7ab622b5b9fc508945b91cfd9189
WongDarren <wongdarren1997@gmail.com>
2020-09-11 17:11:22 -0700

    typo updates

--- src/components/About.js
+++ src/components/About.js
@@ -9,22 +9,25 @@ const About = () => (
         <header className="major">
           <h2>Read about me</h2>
         </header>
-        <h3>Hi! I'm Darren.</h3>
-        <p>
-          I am a 23 year old Las Vegas-based software engineer.
-        </p>
-        <p>
-          I am an alumnus of the University of Nevada, Las Vegas
-          where I graduated in December 2019 with a B.S. in Computer Science.
-        </p>
-        <p>
-          I am currently pursing a career in Front-End Web Development.
-          However, I am open to exploring other opportunities in Software Engineering as earning my degree has taught me how to quickly grasp new ideas and new tools to solve problems.
-        </p>
-        <p>  
-          My works are on my GitHub, where I keep some old coursework and push new personal projects.
-          If you would like to check out my GitHub, click <a href='https://github.com/WongDarren'>here.</a>
-        </p>
+        <div className='bio'>
+          <h3>Hi! I'm Darren.</h3>
+          <p>
+            I am a 23 year old Las Vegas-based software engineer.
+          </p>
+          <p>
+            I am an alumnus of the University of Nevada, Las Vegas
+            where I graduated in December 2019 with a B.S. in Computer Science.
+          </p>
+          <p>
+            I am currently pursing a career in Front-End Web Development.
+            However, I am open to exploring other opportunities in Software Engineering as earning my degree has taught me how to quickly grasp new ideas and new tools to solve problems.
+          </p>
+          <p>  
+            My works are on my GitHub, where I keep some old coursework and push new personal projects.
+            If you would like to check out my GitHub, click <a href='https://github.com/WongDarren'>here.</a>
+          </p>
+        </div>
+        
         {
           /*
             <ul className="actions">
