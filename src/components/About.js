8822ae88c7c113cb1141c1efafe55afe8c17fc53
WongDarren <wongdarren1997@gmail.com>
2021-01-16 17:52:06 -0800

    update portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -9,27 +9,27 @@ const About = () => (
         <header className="major">
           <h2>Read about me</h2>
         </header>
-        <div className='bio'>
+        <div className="bio">
           <h3>Hi! I'm Darren.</h3>
+          <p>I am a 23 year old Las Vegas-based software engineer.</p>
           <p>
-            I am a 23 year old Las Vegas-based software engineer.
+            I am an alumnus of the University of Nevada, Las Vegas where I
+            graduated in December 2019 with a B.S. in Computer Science.
           </p>
           <p>
-            I am an alumnus of the University of Nevada, Las Vegas
-            where I graduated in December 2019 with a B.S. in Computer Science.
+            I am currently pursing a career in Software Engineering. However, I
+            am open to exploring other opportunities in Computer Science as
+            earning my degree has taught me how to quickly grasp new ideas and
+            new tools to solve problems.
           </p>
           <p>
-            I am currently pursing a career in Front-End Web Development.
-            However, I am open to exploring other opportunities in Software Engineering as earning my degree has taught me how to quickly grasp new ideas and new tools to solve problems.
-          </p>
-          <p>  
-            My works are on my GitHub, where I keep some old coursework and push new personal projects.
-            If you would like to check out my GitHub, click <a href='https://github.com/WongDarren'>here.</a>
+            My works are on my GitHub, where I keep some old coursework and push
+            new personal projects. If you would like to check out my GitHub,
+            click <a href="https://github.com/WongDarren">here.</a>
           </p>
         </div>
-        
-        {
-          /*
+
+        {/*
             <ul className="actions">
               <li>
                 <Link to="/generic" className="button">
@@ -37,9 +37,7 @@ const About = () => (
                 </Link>
               </li>
             </ul>
-          */
-        }
-        
+          */}
       </div>
       <span className="image">
         <img src={image} alt="" />
